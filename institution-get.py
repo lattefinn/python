a5de8dd4770fbb84f904cc5b260fc361c59436f5
lattefinn <finn.cook8@gmail.com>
2024-09-30 15:02:04 -0600

    completed csv conversion for institution json data

--- institution-get.py
+++ institution-get.py
@@ -11,35 +11,19 @@ with open('institution-get.json', 'r') as file:
     data = json.load(file)
 
 inst = data["institutions"]
-cleaned = [] 
 print(inst)
-with open('output.csv', 'w', newline='') as csvfile:
+with open('output.csv', 'w', newline='\n') as csvfile:
     writer = csv.writer(csvfile)
-csv_writer=csv.writer(inst)
 
-# counter variable used for writing 
-# headers to the csv file
-
-count = 0
-
-for bank in inst:
-    if count == 0:
-
-        # writing headers of csv file
-        header = inst.keys()
-        # writing data of csv file
-        csv_writer.writerow(header)
-        count += 1
-    print(bank["name"])
-    print(bank["institution_id"])
-    print(bank["oauth"])
-    print("") 
-    name = bank["name"]
-    institution = bank["institution_id"]
-    oauth = bank["oauth"]
-    writer.writerows("data")
-
+    for bank in inst:
     
-    # print(name)
-    # writer.writerow(name)
+        name = bank["name"]
+        institution = bank["institution_id"]
+        oauth = str(bank["oauth"])
+        all = name +  "," + institution + "," + oauth + "\n"
+        writer.writerow([name,institution,oauth])
 
+        
+        # print(name)
+        # writer.writerow(name)
+    csvfile.close()
